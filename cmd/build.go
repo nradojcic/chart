7eb0d50fda68f56c644332d836c06217d4d137fc
Nebojsa <63752654+nradojcic@users.noreply.github.com>
2026-01-07 22:28:16 -0500

    Extract crawling logic to internal/sitemap package

--- cmd/build.go
+++ cmd/build.go
@@ -3,13 +3,9 @@ package cmd
 import (
 	"encoding/xml"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
-	"github.com/nradojcic/chart/internal/link"
+	"github.com/nradojcic/chart/internal/sitemap"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -34,7 +30,7 @@ var buildCmd = &cobra.Command{
 		urlStr := args[0]
 		maxDepth := viper.GetInt("depth")
 
-		pages := bfs(urlStr, maxDepth)
+		pages := sitemap.Crawl(urlStr, maxDepth)
 
 		toXml := urlset{
 			Xmlns: xmlns,
@@ -59,87 +55,4 @@ func init() {
 	buildCmd.Flags().IntP("depth", "d", 3, "The maximum depth to traverse")
 
 	viper.BindPFlag("depth", buildCmd.Flags().Lookup("depth"))
-}
-
-func bfs(urlStr string, maxDepth int) []string {
-	seen := make(map[string]struct{})
-	var q map[string]struct{}
-	nq := map[string]struct{}{
-		urlStr: struct{}{},
-	}
-
-	for i := 0; i <= maxDepth; i++ {
-		q, nq = nq, make(map[string]struct{})
-		if len(q) == 0 {
-			break
-		}
-		for url, _ := range q {
-			if _, ok := seen[url]; ok {
-				continue
-			}
-
-			seen[url] = struct{}{}
-			for _, link := range get(url) {
-				if _, ok := seen[link]; !ok {
-					nq[link] = struct{}{}
-				}
-			}
-		}
-	}
-
-	ret := make([]string, 0, len(seen))
-	for url, _ := range seen {
-		ret = append(ret, url)
-	}
-
-	return ret
-}
-
-func get(urlStr string) []string {
-	resp, err := http.Get(urlStr)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	reqUrl := resp.Request.URL
-	baseUrl := &url.URL{
-		Scheme: reqUrl.Scheme,
-		Host:   reqUrl.Host,
-	}
-	base := baseUrl.String()
-
-	return filter(hrefs(resp.Body, base), withPrefix(base))
-}
-
-func hrefs(r io.Reader, base string) []string {
-	links, _ := link.Parse(r)
-	var ret []string
-	for _, l := range links {
-		switch {
-		case strings.HasPrefix(l.Href, "/"):
-			ret = append(ret, base+l.Href)
-		case strings.HasPrefix(l.Href, "http"):
-			ret = append(ret, l.Href)
-		}
-	}
-
-	return ret
-}
-
-func filter(links []string, keepFn func(string) bool) []string {
-	var ret []string
-	for _, link := range links {
-		if keepFn(link) {
-			ret = append(ret, link)
-		}
-	}
-
-	return ret
-}
-
-func withPrefix(pfx string) func(string) bool {
-	return func(link string) bool {
-		return strings.HasPrefix(link, pfx)
-	}
 }
---
+++ internal/sitemap/sitemap.go
@@ -0,0 +1,95 @@
+package sitemap
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/nradojcic/chart/internal/link"
+)
+
+// Crawl performs a breadth-first crawl of the website starting at urlStr
+// up to maxDepth levels deep, returning a slice of discovered URLs.
+func Crawl(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		if len(q) == 0 {
+			break
+		}
+		for url, _ := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				if _, ok := seen[link]; !ok {
+					nq[link] = struct{}{}
+				}
+			}
+		}
+	}
+
+	ret := make([]string, 0, len(seen))
+	for url, _ := range seen {
+		ret = append(ret, url)
+	}
+
+	return ret
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+}
+
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.Parse(r)
+	var ret []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		}
+	}
+
+	return ret
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
+}
